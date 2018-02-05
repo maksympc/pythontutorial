1da316155522edb49142703f4393d9e75b9f8cce
Maksym Pinchuk <maksympc@gmail.com>
2018-02-05 22:34:55 +0200

    end datacamp chapter 1, variables and types examples

---
+++ datacapm/chapter 1/sc1.py
@@ -0,0 +1,15 @@
+# Chapter 1
+# Suppose, that you have $100, which you can invest with a 10% return each year.
+# How much is your $100 worth after 7 years?
+print(100*1.1**7)
+
+# Variables & Types
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+# Definition of pi_string
+pi_string = "3.1415926"
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
